938d086fca6f55734b3c69c705c844c207a1115a
Katherine <122394999+beijingcorn54@users.noreply.github.com>
2026-05-12 00:17:20 -0400

    Delete helper_functions directory and its contained files

--- helper_functions/getTrials.m
+++
@@ -1,4 +0,0 @@
-function [trials] = getTrials(data)
-    size_vector = size(squeeze(data));
-    trials = size_vector(2);
-end
--- helper_functions/getZForce.m
+++
@@ -1,3 +0,0 @@
-function [z_force] = getZForce(data)
-    z_force = -squeeze(data.forceplates.Force(:, 3, :));
-end
