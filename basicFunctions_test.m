41c75dd4554b2e2557f644dec31515433d931447
Katherine <122394999+beijingcorn54@users.noreply.github.com>
2026-05-12 00:22:17 -0400

    Update basicFunctions_test.m

--- basicFunctions_test.m
+++ basicFunctions_test.m
@@ -2,21 +2,23 @@ clear;
 close all;
 
 % Load in Data
-directory = "/Users/kefuller/Fuller_Locolab_Winter2026/";
+directory = "/Users/kefuller/Fuller_Locolab/";
 dataBase = load(directory + "locolab_files/Normalized.mat").Normalized;
 data = dataBase.AB02.Walk.s1x2.i0;
+legLength = 0.921;
 
 % Plot everything
-threshold = 25;
+force_threshold = 25;
 v_treadmill = -1.2;
-marker = "LHEE";
 incline = 0;
+graphTitle = "AB02.Walk.s1x2.i0";
+test = true;
 
 addpath(directory + 'plotting&testing_functions/');
-plot_forces(data, "AB02.Walk.s1x2.i0", threshold, true, directory);
-plot_strides(data, marker, "AB02.Walk.s1x2.i0", threshold, true, directory);
-plot_corrected_strides(data, marker, v_treadmill, "AB02.Walk.s1x2.i0", threshold, true, directory);
+%plot_forces(data, graphTitle, force_threshold, true, directory);
+%plot_strides(data, graphTitle, force_threshold, test, directory);
+%plot_corrected_strides(data, v_treadmill, graphTitle, force_threshold, test, directory);
 
-display_cadences(data, marker, directory);
-display_stride_lengths(data, threshold, v_treadmill, marker, incline, directory);
+%display_cadences(data, directory);
+%display_stride_lengths(data, v_treadmill, incline, legLength, directory);
 rmpath(directory + 'plotting&testing_functions/');
