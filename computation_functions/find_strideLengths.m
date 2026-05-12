84360cde5f8bfd46e203d0f5b65c5b77ff8b3781
Katherine <122394999+beijingcorn54@users.noreply.github.com>
2026-05-12 00:20:00 -0400

    Update find_strideLengths.m

--- computation_functions/find_strideLengths.m
+++ computation_functions/find_strideLengths.m
@@ -1,24 +1,17 @@
-% Produces stride-lengths (single-leg stride lengths) from all trials of a specified person/incline/speed
-% HS to next HS
+% Produces stride-lengths from all trials of a specified person/incline/speed
 
-function [stride_lengths] = find_strideLengths(data, threshold, v_treadmill, marker, incline, directory)   
-    % Get Position Variables
-    y = squeeze(data.markers.LHEE(:, 2, :));
-
-    addpath(directory + 'helper_functions/');
-    y = correct_strides(y, data, threshold, v_treadmill, marker, directory);
-
-    addpath(directory + 'helper_functions/');
-    y_size = getTrials(y);
-
-    stride_lengths = zeros(1, y_size);
+function [stride_lengths, normalized_stride_lengths] = find_strideLengths(data, v_treadmill, incline, legLength)
+    y = correct_strides(data, v_treadmill);
+    stride_lengths = zeros(1, size(y, 2));
 
     % Make calculations
-    for i = 1 : y_size
+    for i = 1 : size(y, 2)
         stride_lengths(i) = abs(y(150, i) - y(1, i));
     end
 
     % Account for incline
     stride_lengths = stride_lengths ./ cos(incline * pi /180);
 
+    % Produce normalized stride lengths
+    normalized_stride_lengths = stride_lengths ./ legLength;
 end
