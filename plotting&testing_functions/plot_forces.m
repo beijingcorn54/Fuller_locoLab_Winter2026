5216b9aec9e1169063bbc44f84a3aebed07b792e
Katherine <122394999+beijingcorn54@users.noreply.github.com>
2026-05-12 00:21:07 -0400

    Update plot_forces.m

--- plotting&testing_functions/plot_forces.m
+++ plotting&testing_functions/plot_forces.m
@@ -1,30 +1,27 @@
-function plot_forces(data, graphTitle, threshold, test, directory)
+function plot_forces(data, graphTitle, force_threshold, test, directory)
     % Extract Data
     addpath(directory + 'computation_functions/');
-    [HS, TO] = find_HS_TO(data, threshold, directory);
+    [HS, TO] = find_HS_TO(data, force_threshold);
     rmpath(directory + 'computation_functions/');
     
     z_force = -squeeze(data.forceplates.Force(:, 3, :));
 
-    addpath(directory + 'helper_functions/');
-    trials = getTrials(z_force);
-    rmpath(directory + 'helper_functions/');
-    
     % Compute x-axis range
-    z_force_size_vector = size(z_force);
-    z_force_x_axis = 1 : z_force_size_vector(1);
+    z_force_x_axis = 1 : size(z_force, 1);
 
     % Plot One Trial of Forces
     figure;
-    for i = 1: trials
+    for i_col = 1: size(z_force, 2)
         hold on;
-        scatter(z_force_x_axis, z_force(:, i));
+        scatter(z_force_x_axis, z_force(:, i_col));
 
-        scatter(z_force_x_axis(HS(i)), z_force(HS(i), i), 'red', 'o', 'LineWidth', 2);
-        text(z_force_x_axis(HS(i)), z_force(HS(i), i), ['\leftarrow Point' ' Heel-Strike']);
+        scatter(z_force_x_axis(HS(i_col)), z_force(HS(i_col), i_col), 'red', 'o', 'LineWidth', 2);
+        text(z_force_x_axis(HS(i_col)), z_force(HS(i_col), i_col), ['\leftarrow Point' ' Heel-Strike']);
             
-        scatter(z_force_x_axis(TO(i)), z_force(TO(i), i), 'red', 'o', 'LineWidth', 2);
-        text(z_force_x_axis(TO(i)), z_force(TO(i), i), ['\leftarrow Point' ' Toe-Off']);
+        scatter(z_force_x_axis(TO(i_col)), z_force(TO(i_col), i_col), 'red', 'o', 'LineWidth', 2);
+        text(z_force_x_axis(TO(i_col)), z_force(TO(i_col), i_col), ['\leftarrow Point' ' Toe-Off']);
+
+        plot(1 : 150, ones(1, 150) * 25);
 
         grid on;
         xlabel('X-AXIS (unknownUnits)');
@@ -37,20 +34,22 @@ function plot_forces(data, graphTitle, threshold, test, directory)
 
     % TEST: PLOT ALL INDIVIDUAL TRIALS
     if test
-        for i = 1: trials
+        for i_col = 1: size(z_force, 2)
             figure;
-            scatter(z_force_x_axis, z_force(:, i));
+            scatter(z_force_x_axis, z_force(:, i_col));
             hold on;
-            scatter(z_force_x_axis(HS(i)), z_force(HS(i), i), 'red', 'o', 'LineWidth', 2);
-            text(z_force_x_axis(HS(i)), z_force(HS(i), i), ['\leftarrow Point' ' Heel-Strike']);
+            scatter(z_force_x_axis(HS(i_col)), z_force(HS(i_col), i_col), 'red', 'o', 'LineWidth', 2);
+            text(z_force_x_axis(HS(i_col)), z_force(HS(i_col), i_col), ['\leftarrow Point' ' Heel-Strike']);
             
-            scatter(z_force_x_axis(TO(i)), z_force(TO(i), i), 'red', 'o', 'LineWidth', 2);
-            text(z_force_x_axis(TO(i)), z_force(TO(i), i), ['\leftarrow Point' ' Toe-Off']);
+            scatter(z_force_x_axis(TO(i_col)), z_force(TO(i_col), i_col), 'red', 'o', 'LineWidth', 2);
+            text(z_force_x_axis(TO(i_col)), z_force(TO(i_col), i_col), ['\leftarrow Point' ' Toe-Off']);
+
+            plot(1 : 150, ones(1, 150) * 25);
             
             grid on;
             xlabel('X-AXIS (unknownUnits)');
             ylabel('FORCE (N)');
-            title(graphTitle + " SINGLE FORCE PLOT OF TRIAL " + i);
+            title(graphTitle + " SINGLE FORCE PLOT OF TRIAL " + i_col);
             xlim([-10, z_force_x_axis(end) + 10]);
             ylim([-50, 900]);
             hold off;
