079ac489728195873157831d43fcb048e55d5491
Katherine <122394999+beijingcorn54@users.noreply.github.com>
2026-05-12 00:19:45 -0400

    Update find_HS_TO.m

--- computation_functions/find_HS_TO.m
+++ computation_functions/find_HS_TO.m
@@ -1,17 +1,39 @@
 % Produces HS/TO indecies from all trials of a specified person/incline/speed 
-function [HS, TO] = find_HS_TO(data, threshold, directory)
+function [HS_vector, TO_vector] = find_HS_TO(data, force_threshold)
     % Extract Data
-    addpath(directory + 'helper_functions/');
-    z_force = getZForce(data);
-    num_trials = getTrials(z_force);
+    z_force = -squeeze(data.forceplates.Force(:, 3, :));
 
-    HS = zeros(1, num_trials);
-    TO = zeros(1, num_trials);
+    % Initialize HS and TO vectors
+    HS_vector = zeros(1, size(z_force, 2));
+    TO_vector = HS_vector;
 
-    % Produce HS and TO vectors
-    for i = 1 : num_trials
-        HS(i) = findMin_index(1, 75, z_force(:, i), threshold);
-        TO(i) = findMin_index(76, 125, z_force(:, i), threshold);
+    %% Produce HS and TO vectors
+    for i_col = 1 : size(z_force, 2)
+        this_HS = 1;
+        this_TO = 150;
+
+        upper_threshold = 1000000;
+        lower_threshold = 1000000;
+
+        for i_row = 1 : 120
+            this_datum = z_force(i_row, i_col);
+
+            if (i_row < 30) && ...
+                (abs(this_datum - force_threshold) < abs(lower_threshold - force_threshold))
+                lower_threshold = this_datum;
+                this_HS = i_row;
+            end
+
+            if (i_row > 30) && ...
+                (abs(this_datum - force_threshold) < abs(upper_threshold - force_threshold))
+                upper_threshold = this_datum;
+                this_TO = i_row;
+            end
+
+        end
+
+        % Append to HS and TO vectors
+        HS_vector(i_col) = this_HS;
+        TO_vector(i_col) = this_TO;
     end
-    rmpath(directory + 'helper_functions/');
 end
