ea151ee6503e52a32a27558ca611d46cba3fb8d4
Katherine <122394999+beijingcorn54@users.noreply.github.com>
2026-05-12 00:17:47 -0400

    Delete computation_functions/find_normalized_strideLengths.m

--- computation_functions/find_normalized_strideLengths.m
+++
@@ -1,6 +0,0 @@
-% Produces normalized stride-lengths (single-leg stride lengths) from all trials of a specified person/incline/speed
-
-function [stride_lengths] = find_normalized_strideLengths(data, threshold, v_treadmill, marker, incline, legLength, directory)   
-    stride_lengths = find_strideLengths(data, threshold, v_treadmill, marker, incline, directory);
-    stride_lengths = stride_lengths ./ legLength;
-end
