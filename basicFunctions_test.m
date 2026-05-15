108a65eb9b55cb54e39e7e91b617b7aa9110cbed
Katherine <122394999+beijingcorn54@users.noreply.github.com>
2026-05-15 12:56:36 -0400

    Exports Figures to a Single PDF file

--- basicFunctions_test.m
+++ basicFunctions_test.m
@@ -15,9 +15,25 @@ graphTitle = "AB02.Walk.s1x2.i0";
 test = true;
 
 addpath(directory + 'plotting&testing_functions/');
-%plot_forces(data, graphTitle, force_threshold, true, directory);
-%plot_strides(data, graphTitle, force_threshold, test, directory);
-%plot_corrected_strides(data, v_treadmill, graphTitle, force_threshold, test, directory);
+plot_forces(data, graphTitle, force_threshold, true, directory);
+plot_strides(data, graphTitle, force_threshold, test, directory);
+plot_corrected_strides(data, v_treadmill, graphTitle, force_threshold, test, directory);
 
-%display_cadences(data, directory);
-%display_stride_lengths(data, v_treadmill, incline, legLength, directory);
+display_cadences(data, directory);
+display_stride_lengths(data, v_treadmill, incline, legLength, directory);
+
+% Export to a PDF
+figs = findall(groot, 'Type', 'figure');
+pdf_file_name = 'Basic Functions Test.pdf';
+
+if isfile(pdf_file_name)
+    delete(pdf_file_name)
+end
+
+for i = 1 : length(figs)
+    if i == 1 % First page creates the PDF
+        exportgraphics(figs(i), pdf_file_name,'ContentType', 'vector');
+    else
+        exportgraphics(figs(i), pdf_file_name, 'ContentType', 'vector', 'Append', true);
+    end
+end
