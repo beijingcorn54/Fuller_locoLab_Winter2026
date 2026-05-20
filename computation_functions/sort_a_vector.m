4490f10590541e0841a97ddb71b114ab6c94c94c
Katherine <122394999+beijingcorn54@users.noreply.github.com>
2026-05-20 16:29:37 -0400

    Update sort_a_vector.m

--- computation_functions/sort_a_vector.m
+++ computation_functions/sort_a_vector.m
@@ -6,25 +6,15 @@
 
 % Keeps those measurements ingrained in the data
 
-function [sorted_data] = sort_a_vector(data, upper_bound, lower_bound, sorting_type)
+function [sorted_data] = sort_a_vector(data, upper_bound, lower_bound, sorting_row)
     sorted_data = [];
-    if sorting_type == "c"
-        row = 1;
-    elseif sorting_type == "nsl"
-        row = 2;
-    elseif sorting_type == "cs"
-        row = 3;
-    elseif sorting_type == "rs"
-        row = 4;
-    end
-
     for i_col = 1 : size(data, 2)
 
         data_to_append = data(:, i_col);
-        sorting_measurement = data_to_append(row);        
+        sorting_measurement = data_to_append(sorting_row);        
     
         valid_entry = ~isnan(data_to_append(10)); % Hardcoded, assumed that no more than 10 measurements need to be labled within the data
-        within_upper_bound = sorting_measurement < upper_bound;
+        within_upper_bound = (sorting_measurement < upper_bound);
         within_lower_bound = (sorting_measurement > lower_bound) || (sorting_measurement == lower_bound);
         
        
