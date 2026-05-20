73cd9ba70b8a965c203ca2cf61008c1262e608ee
Katherine <122394999+beijingcorn54@users.noreply.github.com>
2026-05-20 16:29:04 -0400

    Update get_mean_std_vector.m

--- computation_functions/get_mean_std_vector.m
+++ computation_functions/get_mean_std_vector.m
@@ -1,18 +1,15 @@
+% Output does not include the ingrained measurements from the dataset
+
 function [mean_std_vector] = get_mean_std_vector(data)
     mean_std_vector = [];
 
-    if ~isempty(data(5 : end)) % Hardcoded based on the number of ingrained measurements
+    if ~isempty(data)     
         % Interpolate Data
-        interpolated_data = get_interpolated_data(data);
+        interpolated_data = get_interpolated_data(data(5 : end, :)); % Hardcoded based on the number of ingrained measurements
     
-        % Find mean, standard deviation, and number of trials/steps represented in the matrix
-        mean_std_vector = [];
-    
+        % Find mean, standard deviation, and number of trials/steps represented in the matrix    
         for i_row = 1 : size(interpolated_data, 1)        
             mean_std_vector = [mean_std_vector; [mean(interpolated_data(i_row, :)), std(interpolated_data(i_row, :)), size(interpolated_data, 2)]];
-        end
-
-        % Remove the ingrained measurements from the dataset
-        mean_std_vector = mean_std_vector(5 : end, :); % Hardcoded based on the number of ingrained measurements
+        end        
     end
 end
